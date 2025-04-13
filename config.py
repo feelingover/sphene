3cbcba3e7385f06b9cc65bd93d05bf29d4597950
feelingover <1415534+feelingover@users.noreply.github.com>
2025-04-13 22:09:11 +0900

    ✨ キャラクターをアサヒに変更

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 OPENAI_API_KEY: str = str(os.getenv("OPENAI_API_KEY"))
 DISCORD_TOKEN: str = str(os.getenv("DISCORD_TOKEN"))
-BOT_NAME: str = str(os.getenv("BOT_NAME", "スフェーン"))
-COMMAND_GROUP_NAME: str = str(os.getenv("COMMAND_GROUP_NAME", "sphene"))
+BOT_NAME: str = str(os.getenv("BOT_NAME", "アサヒ"))
+COMMAND_GROUP_NAME: str = str(os.getenv("COMMAND_GROUP_NAME", "asahi"))
 
 # 許可されたチャンネルのID（環境変数から取得、カンマ区切りで設定）
 ALLOWED_CHANNEL_IDS: List[int] = []
